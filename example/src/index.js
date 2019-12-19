63601b364745781a7cc01ef9ade2b9b8976226c9
Kishore Patra <kishorekumar@nodetech.in>
2019-12-19 17:44:51 +0530

    init

---
+++ example/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RCTXContextenu from 'rctx-contextmenu';
+
+const Demo = () => {
+    return (
+        <div>
+            <RCTXContextenu>Right click on me</RCTXContextenu>
+        </div>
+    )
+}
+
+ReactDOM.render(<Demo />, document.getElementById('root'));
